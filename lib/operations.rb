afd4d0aaf0be1ec4760a183ea2c60aae647ddf26
Karen V <karenvo@usc.edu>
2019-02-07 20:55:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-	if speed < 40 and speed > 60
+	if speed < 40 or speed > 60
 		true
 	else
 		false
@@ -9,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-
+	speed < 40 or speed > 60 ? true:false
 end
